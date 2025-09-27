5538d3ca2b068c2e44f7223638344b6075f9163a
CynthiaKitners <CKitners@uccs.edu>
2025-09-27 20:23:21 +0000

    2nd aattempt to commit

--- movie_theater_booking/bookings/models.py
+++ movie_theater_booking/bookings/models.py
@@ -1,43 +1,27 @@
-import os
-import django
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookings.settings")
-# django.setu
-
-
-from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
 class Movie(models.Model):
-    title = models.CharField(max_length=200) 
-    description = models.TextField(blank=True, null=True)
-    release_date = models.DateField() 
-    duration = models.PositiveIntegerField(help_text="Duration in minutes")
-    
-    def __str__(self):
-        return self.title
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    release_date = models.DateField()    
+    duration = models.DurationField(help_text="Duration of the movie (HH:MM:SS)")
 
+#==============================================================================
 
-class Seats(models.Model):
-    seat_number = models.CharField(max_length=10, unique=True)  # e.g., "A1", "B3"
-    booking_status = models.BooleanField(default=False)  # True if booked
-  
-    def __str__(self):
-        return f"Seat {self.seat_number} ({'Reserved' if self.is_reserved else 'Available'})"
-
+class Seat(models.Model)        :
+    seat_number = models.CharField(max_length = 20)
+    is_seat_booked =  models.BooleanField(default = False)
+ 
+#==============================================================================        
 
 class Booking(models.Model):
-    movie_name = models.CharField(max_length=100)
-    seat = models.OneToOneField(  # One seat per booking
-        Seat,
-        on_delete=models.CASCADE,
-        related_name="booking"
-    )
-    booking_date = models.DateTimeField(auto_now_add=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    booking_date = models.DateTimeField(auto_now_add = True)
 
-    def __str__(self):
-        return f"Booking for {self.customer_name} - {self.seat.seat_number}"
-
-        
-
-
+    
